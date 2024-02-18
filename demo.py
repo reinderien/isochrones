e81f4830c49919d17581b402a240262aefad794d
reinderien <gtoombs@lakeheadu.ca>
2024-02-18 16:23:46 -0500

    hemisphere classes

--- demo.py
+++ demo.py
@@ -1,12 +1,13 @@
 import json
+from typing import NamedTuple
 
-import cartopy
-from cartopy.feature.nightshade import Nightshade
-from cartopy.mpl.geoaxes import GeoAxes
+import cartopy.crs
+import cartopy.feature.nightshade
+import cartopy.mpl.geoaxes
+import matplotlib.patches
 from matplotlib import pyplot as plt
-from matplotlib.patches import Arc
 
-KAABA = (39.826167, 21.4225)
+KAABA_COORD = (39.826167, 21.4225)
 
 
 def load_home() -> tuple[float, float]:
@@ -15,64 +16,103 @@ def load_home() -> tuple[float, float]:
     return coords['lon'], coords['lat']
 
 
-def main() -> None:
-    home = load_home()
+class Hemisphere(NamedTuple):
+    name: str
+    coord: tuple[float, float]
+    crs: cartopy.crs.Orthographic
+    ax: cartopy.mpl.geoaxes.GeoAxes
 
-    crs_home = cartopy.crs.Orthographic(
-        central_longitude=home[0],
-        central_latitude=home[1],
-    )
-    sphere_globe = crs_home.globe
-    geodetic = cartopy.crs.Geodetic(globe=sphere_globe)
+    GEODETIC_COLOUR = 'green'
+    FEATURE_COLOUR = 'black'
 
-    crs_kabaa = cartopy.crs.Orthographic(
-        central_longitude=KAABA[0],
-        central_latitude=KAABA[1],
-        globe=sphere_globe,
-    )
+    @classmethod
+    def make(
+        cls,
+        name: str,
+        index: int,
+        coord: tuple[float, float],
+        figure: plt.Figure,
+        n_axes: int = 2,
+        globe: cartopy.crs.Globe | None = None,
+    ) -> 'Hemisphere':
+        crs = cartopy.crs.Orthographic(
+            central_longitude=coord[0],
+            central_latitude=coord[1],
+            globe=globe,
+        )
+        ax = figure.add_subplot(1, n_axes, index, projection=crs)
+        return cls(name=name, coord=coord, crs=crs, ax=ax)
 
-    # higher-resolution geodesics - not needed for orthographic since geodesics are straight
-    # crs_home.threshold *= 0.1
-    # crs_kabaa.threshold *= 0.1
+    def plot(
+        self,
+        endpoint: tuple[float, float],
+        geodetic: cartopy.crs.Geodetic,
+        night: cartopy.feature.nightshade.Nightshade,
+    ) -> None:
+        self.ax.set_title(f'{self.name} hemisphere')
 
-    geodesic_azimuth = 10  # bad
+        self.ax.stock_img()
+        self.ax.plot(
+            [self.coord[0], endpoint[0]],
+            [self.coord[1], endpoint[1]],
+            transform=geodetic, c=self.GEODETIC_COLOUR,
+        )
+        self.ax.add_feature(night)
+        self.ax.gridlines()
+        self.ax.scatter(
+            [self.coord[0]], [self.coord[1]],
+            transform=geodetic, c=self.FEATURE_COLOUR, marker='+', zorder=10,
+        )
 
+
+class HeadingHemisphere(Hemisphere):
+    HEADING_COLOUR = 'red'
+
+    def plot_heading(
+        self,
+        geodetic: cartopy.crs.Geodetic,
+        geodesic_azimuth: float,
+    ) -> None:
+        north = 90
+        self.ax.plot(
+            [self.coord[0], self.coord[0]],
+            [self.coord[1], self.coord[1] + 20],
+            transform=geodetic, c=self.HEADING_COLOUR,
+        )
+        self.ax.add_patch(matplotlib.patches.Arc(
+            xy=self.coord, transform=geodetic, color=self.HEADING_COLOUR,
+            width=10, height=10, theta1=geodesic_azimuth, theta2=north,
+        ))
+
+
+def plot(
+    home_coord: tuple[float, float],
+    geodesic_azimuth: float,
+) -> plt.Figure:
     fig: plt.Figure = plt.figure()
-    axes: list[GeoAxes] = [
-        fig.add_subplot(1, 2, i, projection=crs)
-        for i, crs in enumerate((crs_home, crs_kabaa), start=1)
-    ]
-    ax_home, ax_kabaa = axes
-    ax_home.set_title('Home hemisphere')
-    ax_kabaa.set_title('Kabaa hemisphere')
 
-    heading_colour = 'red'
-    geodetic_colour = 'green'
-    feature_colour = 'black'
+    home_hemi = HeadingHemisphere.make(
+        name='Home', index=1, coord=home_coord, figure=fig)
+    kaaba_hemi = Hemisphere.make(
+        name='Kaaba', index=2, coord=KAABA_COORD, figure=fig, globe=home_hemi.crs.globe)
 
-    night = Nightshade()
+    geodetic = cartopy.crs.Geodetic(globe=home_hemi.crs.globe)
+    night = cartopy.feature.nightshade.Nightshade()
 
-    for ax, origin in zip(axes, (home, KAABA)):
-        ax.stock_img()
-        ax.plot(
-            [home[0], KAABA[0]], [home[1], KAABA[1]], transform=geodetic, c=geodetic_colour,
-        )
-        ax.add_feature(night)
-        ax.gridlines()
-        ax.scatter(
-            [origin[0]], [origin[1]], transform=geodetic, c=feature_colour, marker='+', zorder=10,
-        )
+    kaaba_hemi.plot(endpoint=home_hemi.coord, geodetic=geodetic, night=night)
+    home_hemi.plot(endpoint=kaaba_hemi.coord, geodetic=geodetic, night=night)
+    home_hemi.plot_heading(geodetic=geodetic, geodesic_azimuth=geodesic_azimuth)
 
-    north = 90
-    ax_home.plot(
-        [home[0], home[0]],
-        [home[1], home[1] + 20],
-        transform=geodetic, c=heading_colour,
+    return fig
+
+
+def main() -> None:
+    home_coord = load_home()
+
+    plot(
+        home_coord=home_coord,
+        geodesic_azimuth=10,  # wrong
     )
-    ax_home.add_patch(Arc(
-        xy=home, transform=geodetic, color=heading_colour,
-        width=10, height=10, theta1=geodesic_azimuth, theta2=north,
-    ))
 
     plt.show()
 
