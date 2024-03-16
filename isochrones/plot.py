10e3b1f0a9a039228e401cf7f82f9a3f220fcaef
reinderien <gtoombs@lakeheadu.ca>
2024-03-16 08:59:21 -0400

    WIP - animation

--- isochrones/plot.py
+++ isochrones/plot.py
@@ -23,7 +23,7 @@ def time_colour(is_night: bool, colours: tuple[str, str]) -> str:
     return night if is_night else day
 
 
-class Hemisphere(NamedTuple):
+class HemisphereData(NamedTuple):
     """
     One hemisphere for graphing. Assumed spherical and not oblate because the orthographic
     projection only accepts spherical. Printed figures like the inverse geodesic still use the more
@@ -33,27 +33,24 @@ class Hemisphere(NamedTuple):
     name: str                      # Hemisphere name, English or romanized Arabic
     coord: tuple[float, float]     # "here" lon, lat in degrees; hemisphere centred on this
     endpoint: tuple[float, float]  # "there" lon, lat in degrees; geodesic heads there
+    home: tuple[float, float]
     crs: Orthographic              # Projective coordinate system for graphing
     geodetic: Geodetic             # Globe coordinate system; this one is spherical
-    ax: GeoAxes                    # Plot on this
     timezone: tzinfo | None        # None means local timezone. Used for date display.
+    include_heading: bool
 
     @classmethod
     def make(
         cls,
         name: str,
-        index: int,
         coord: tuple[float, float],
         endpoint: tuple[float, float],
-        figure: plt.Figure,
         local_timezone: tzinfo | None,
-        n_axes: int = 2,
         geodetic: Geodetic | None = None,
-    ) -> 'Hemisphere':
+        parallel_on_self: bool = False,
+        include_heading: bool = False,
+    ) -> 'HemisphereData':
         """
-        :param index: of the axes within the figure.
-        :param figure: to which the new axes will be added
-        :param n_axes: in the figure. Typically two, for two hemispheres.
         :param geodetic: if None, then a spherical geodetic will be made.
         :return: A new Hemisphere.
         """
@@ -61,36 +58,68 @@ class Hemisphere(NamedTuple):
             central_longitude=coord[0],
             central_latitude=coord[1], globe=geodetic and geodetic.globe,
         )
-        ax = figure.add_subplot(1, n_axes, index, projection=crs)
         return cls(
-            name=name, coord=coord, endpoint=endpoint, crs=crs, ax=ax, timezone=local_timezone,
-            geodetic=geodetic or Geodetic(globe=crs.globe))
+            name=name, coord=coord, endpoint=endpoint, crs=crs, timezone=local_timezone,
+            geodetic=geodetic or Geodetic(globe=crs.globe),
+            home=coord if parallel_on_self else endpoint,
+            include_heading=include_heading,
+        )
 
-    def plot(
-        self,
-        dusk: Nightshade,
-        night: Nightshade,
-        utcnow: datetime,
-        include_heading: bool = False,
-        parallel_on_self: bool = False,
-    ) -> None:
-        local_now = utcnow.astimezone(self.timezone)
-        is_night = check_contains_night(night=dusk, local_now=local_now, coord=self.coord)
-        home = self.coord if parallel_on_self else self.endpoint
 
-        self.plot_common(dusk=dusk, night=night, local_now=local_now, is_night=is_night)
-        self.plot_ecliptic_parallel(home=home, utcnow=utcnow)
-        self.plot_prayer_isochrones(utcnow=utcnow)
-        if include_heading:
-            self.plot_heading(is_night=is_night)
+class HemispherePlots(NamedTuple):
+    data: HemisphereData
+    ax: GeoAxes                    # Plot on this
 
-    def plot_common(
-        self,
-        dusk: Nightshade,
-        night: Nightshade,
-        local_now: datetime,
-        is_night: bool,
-    ) -> None:
+    @classmethod
+    def make(
+        cls,
+        data: HemisphereData,
+        figure: plt.Figure,
+        index: int,
+        n_axes: int = 2,
+    ) -> 'HemispherePlots':
+        """
+        :param data:
+        :param index: of the axes within the figure.
+        :param figure: to which the new axes will be added
+        :param n_axes: in the figure. Typically two, for two hemispheres.
+        :return:
+        """
+        ax = figure.add_subplot(nrows=1, ncols=n_axes, index=index, projection=data.crs)
+
+        return cls(
+        )
+
+    @classmethod
+    def setup_invariants(cls) -> None:
+        self.ax.set_title(f'{self.name} hemisphere')
+        self.heading()
+
+    #
+    #
+    # @classmethod
+    # def plot(
+    #     cls,
+    #     # dusk: Nightshade,
+    #     # night: Nightshade,
+    #     # utcnow: datetime,
+    # ) -> None:
+    #     # local_now = utcnow.astimezone(self.timezone)
+    #     # is_night = check_contains_night(night=dusk, local_now=local_now, coord=self.coord)
+    #
+    #     self.plot_common(dusk=dusk, night=night, local_now=local_now, is_night=is_night)
+    #     self.plot_ecliptic_parallel(home=home, utcnow=utcnow)
+    #     self.plot_prayer_isochrones(utcnow=utcnow)
+    #     if include_heading:
+    #         self.plot_heading(is_night=is_night)
+    #
+    # def plot_common(
+    #     self,
+    #     dusk: Nightshade,
+    #     night: Nightshade,
+    #     local_now: datetime,
+    #     is_night: bool,
+    # ) -> None:
         """
         Plot the common elements: the surface bitmap, night shading, the geodetic between the prayer
         location and the Kaaba, and the time at the centre.
@@ -98,7 +127,6 @@ class Hemisphere(NamedTuple):
         :param night: to shade full darkness; high refractive correction
         """
 
-        self.ax.set_title(f'{self.name} hemisphere')
         self.ax.stock_img()
         self.ax.add_feature(dusk, zorder=5)
         self.ax.add_feature(night, zorder=5)
@@ -199,12 +227,12 @@ def plot_spherical(
     plt.style.use('dark_background')
     fig: plt.Figure = plt.figure()
 
-    home_hemi = Hemisphere.make(
+    home_data = HemisphereData.make(
         name='Home', index=1, coord=home_coord, endpoint=KAABA_COORD,
         figure=fig, local_timezone=None)
-    kaaba_hemi = Hemisphere.make(
+    kaaba_data = HemisphereData.make(
         name='Kaaba', index=2, coord=KAABA_COORD, endpoint=home_coord,
-        figure=fig, local_timezone=KAABA_TIMEZONE, geodetic=home_hemi.geodetic)
+        figure=fig, local_timezone=KAABA_TIMEZONE, geodetic=home_data.geodetic)
 
     # this angle should be made to match the angles in the prayer database
     night = Nightshade(date=utcnow, delta=2, refraction=-15, alpha=0.33)
